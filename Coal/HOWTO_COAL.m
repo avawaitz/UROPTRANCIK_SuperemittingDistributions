461ab9ae3ad3bbe425453a77457f1746fb5e068a
AvaWaitz <avawaitz@mit.edu>
2019-11-18 19:10:51 -0500

    Update HOWTO_COAL.m

--- Coal/HOWTO_COAL.m
+++ Coal/HOWTO_COAL.m
@@ -2,38 +2,12 @@
 HOWTO_COAL
 
 Import Generator and BoilerFuel and BOILER_GENERATOR excel files for each year as Tables
-Import UnitConversion as table
+Import EmissionsUnitConversion excel file as table
 Convert Tables to Structs with table2struct
 assocStruct = BGaggregate(BOILER_GENERATORStruct)
 yearDataStruct = cleanCoalData(generatorStruct,boilerStruct,unitStruct,assocStruct)
-HeatRates_Year = calcHeatRates(yearDataStruct)
-coalDataPlots(Title,HeatRates_Year,numberBins)
-
-
-
-QUANTITY:
-Units: thousand tons
-BIT 
-LIG
-SUB
-WC
-PC
-
-Units:thousand barrels
-DFO
-JF
-KER
-RFO
-WO
-
-Units: thousand cubic feet
-NG
-
-HEAT CONTENT:
-solids: Btu/lb
-liquids: Btu/gallon
-gases: Btu/cubic ft
-
+EmissionRatesStruct = calcHeatRates(yearDataStruct)
+coalDataPlots(Title,[EmissionRatesStruct.EMISSION_RATE],numberBins)
 
 *need to remove upper outliers
 %}
